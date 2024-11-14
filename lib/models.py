fb4dcc0cce55185e733c575027f10f1d0ae9e1ae
michaelanbrown <m_brownbrown@yahoo.com>
2024-11-14 15:16:03 -0500

    added provided users code

--- lib/models.py
+++ lib/models.py
@@ -39,3 +39,17 @@ class Review(Base):
         return f'Review(id={self.id}, ' + \
             f'score={self.score}, ' + \
             f'game_id={self.game_id})'
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+
+    # don't forget your __repr__()!
+    def __repr__(self):
+        return f'User(id={self.id}, ' + \
+            f'name={self.name})'
+    
